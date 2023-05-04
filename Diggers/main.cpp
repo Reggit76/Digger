dbe9a70b2e38468671ad203fb91362af7f4f4c89
yakyw <132575118+yakyw@users.noreply.github.com>
2023-05-04 16:16:45 +0300

    изменения

--- Diggers/main.cpp
+++ Diggers/main.cpp
@@ -1,21 +1,5 @@
-#include <SFML/Graphics.hpp>
-
+#include<iostream>
 int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Digger");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
-
-    while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+	std::cout << "Hello World";
+	return 0;
 }
