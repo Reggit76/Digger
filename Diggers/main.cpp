991c5df70fa33f642a7b9958e9aeb7924b4b4210
Reggit76 <kots.genrix76@mail.ru>
2023-05-04 16:08:50 +0300

    new commit

--- Diggers/main.cpp
+++ Diggers/main.cpp
@@ -2,16 +2,23 @@
 
 int main() {
     sf::RenderWindow window(sf::VideoMode(800, 600), "Digger");
-    sf::CircleShape shape(100.f);
+    sf::CircleShape shape(50.f);
     shape.setFillColor(sf::Color::Green);
-
+    int posX = 0, posY = 0;
     while (window.isOpen()) {
         sf::Event event;
+        
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed)
                 window.close();
+            if (event.type == sf::Event::KeyPressed) {
+                if (event.key.code == sf::Keyboard::Left) posX--;
+                if (event.key.code == sf::Keyboard::Right) posX++;
+                if (event.key.code == sf::Keyboard::Down) posY++;
+                if (event.key.code == sf::Keyboard::Up) posY--;
+            }
         }
-
+        shape.setPosition(posX * 25, posY * 25);
         window.clear();
         window.draw(shape);
         window.display();
